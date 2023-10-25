214797bb7e4acfb3a936a7eaea6c58132dd33a42
Borys Pedorenko <boriszhneya@gmail.com>
2023-10-25 18:47:17 +0200

    First commit

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    static int idPerson;
+    int age;
+    String name;
+
+    public Person(int age, String name, double oklad, int stagRabotyLet) {
+        this.age = age;
+        this.name = name;
+        this.oklad = oklad;
+        this.stagRabotyLet = stagRabotyLet;
+    }
+
+    double   oklad;
+    int stagRabotyLet;
+    double raschenZP(double oklad, int stagRabotyLet){
+        return oklad+oklad*stagRabotyLet/10;
+    }
+    void generationSms(String name, double oklad, int stagRabotyLet){
+        System.out.println("Сотруднику "+idPerson+" "+name+" насчитано зарплаты - "+ raschenZP(oklad,stagRabotyLet)+"$");
+    }
+}
