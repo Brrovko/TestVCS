50ae6f050791f53e5d89d3bb35faf2692eac686a
Andrew Zola <and.zola.77@gmail.com>
2023-10-25 19:22:31 +0200

    My commit with Person

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,8 @@
-public class Student {
+public class Student extends Person{
+
+    public Student(int age, String name, double oklad, int stagRabotyLet) {
+        super(age, name, oklad, stagRabotyLet);
+    }
 
     public void study(){
         System.out.println("I am studying");
